d7252d6d51aa8f7748acd7a94e69763041ecff55
Edwin Steiner <edwin.steiner@gmx.net>
2021-12-08 19:34:41 +0100

    directory setup

--- make.bat
+++ make.bat
@@ -2,7 +2,7 @@
 rem This batch file is intended to be used from vim with 'set makeprg=cmd\ /c\ make.bat'
 rem You will need to adapt the path below to your environment.
 
-call D:\Programs\VS_IDE\Common7\Tools\VsDevCmd.bat -arch=amd64 -host_arch=amd64
+call %MY_VISUAL_STUDIO_IDE_DIR%\Common7\Tools\VsDevCmd.bat -arch=amd64 -host_arch=amd64
 
 set DIR=%~dp0
 echo Entering directory %DIR%
